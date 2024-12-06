8e7577c182869aed7d43c7cf9c5fc54e99f6d636
abdelillahbel <73066920+abdelillahbel@users.noreply.github.com>
2024-12-06 21:57:06 +0100

    add AppTexts widget for consistent styling

---
+++ lib/core/widgets/app_texts.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppTexts extends StatelessWidget {
+  const AppTexts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      width: 40,
+      color: Colors.black,
+    );
+  }
+}
