d6b58805581367253b748f77705a6323bf33a4d4
abdelillahbel <73066920+abdelillahbel@users.noreply.github.com>
2024-12-11 17:29:46 +0100

    feat: add ticketList data structure for flight information

---
+++ lib/core/data/tickets_data.dart
@@ -0,0 +1,30 @@
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {
+      'code':"ORN",
+      'name':"Oran"
+    },
+    'to': {
+      'code':"DXB",
+      'name':"Dubai"
+    },
+    'flying_time': '9H 30M',
+    'date': "01 FEB",
+    'departure_time':"07:00 AM",
+    "number":19
+  },
+  {
+    'from': {
+      'code':"ALG",
+      'name':"Algers"
+    },
+    'to': {
+      'code':"SAN",
+      'name':"Shanghai"
+    },
+    'flying_time': '4H 20M',
+    'date': "10 MAY",
+    'departure_time':"09:00 AM",
+    "number":45
+  },
+];
