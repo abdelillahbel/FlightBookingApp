aae777955aa91ccbb7896612979f2165af754c5f
abdelillahbel <73066920+abdelillahbel@users.noreply.github.com>
2024-12-22 23:46:10 +0100

    feat: add mastercard image to media assets

--- lib/core/res/media.dart
+++ lib/core/res/media.dart
@@ -1,8 +1,9 @@
 class AppMedia {
   static const _baseImage = "assets/images";
   static const logo = '$_baseImage/logo.png';
-  static const tickets = '$_baseImage/tickets.png';
   static const airplan1 = '$_baseImage/airplan1.png';
+  static const tickets = '$_baseImage/tickets.png';
+  static const mastercard = '$_baseImage/mastercard.png';
   static const airalgeria = '$_baseImage/airalgeria.png';
   static const hotelRoom = '$_baseImage/hotel_room.png';
   static const emojiLoveEyes = '$_baseImage/smiling-face-with-heart-shaped-eyes.png';
