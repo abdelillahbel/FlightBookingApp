15aba8945e3364f918bbdde3b2c5eccb8f672fad
abdelillahbel <73066920+abdelillahbel@users.noreply.github.com>
2024-12-27 20:09:32 +0100

    refactor: optimize layout in TicketView and TicketScreen

--- lib/screens/ticket/ticket_screen.dart
+++ lib/screens/ticket/ticket_screen.dart
@@ -17,184 +17,215 @@ class TicketScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppTheme.bgColor,
-      body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+      body: Stack(
         children: [
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Tickets",
-            style: AppTheme.headLineStyle1,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TicketsTabs(
-            firstTab: "Upcoming",
-            secondTab: "Previous",
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          // flight details ticket
-          Container(
-            child: Column(
-              children: [
-                TicketView(
-                  ticket: ticketList[0],
-                  wholeScreen: true,
-                  primaryColor: Colors.black,
-                  secondaryColor: Colors.grey.shade700,
-                  designColor: Colors.lightBlue.shade200,
-                  circleColor: Colors.white,
-                  styleTwo: true,
-                ),
-                SizedBox(
-                  height: 1,
-                ),
-                Container(
-                  width: 360,
-                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                  color: Colors.white,
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          ListView(
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Tickets",
+                style: AppTheme.headLineStyle1,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TicketsTabs(
+                firstTab: "Upcoming",
+                secondTab: "Previous",
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              // flight details ticket
+              Container(
+                child: Column(
+                  children: [
+                    TicketView(
+                      ticket: ticketList[0],
+                      wholeScreen: true,
+                      primaryColor: Colors.black,
+                      secondaryColor: Colors.grey.shade700,
+                      designColor: Colors.lightBlue.shade200,
+                      circleColor: Colors.white,
+                      styleTwo: true,
+                    ),
+                    SizedBox(
+                      height: 1,
+                    ),
+                    Container(
+                      width: 360,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                      color: Colors.white,
+                      child: Column(
                         children: [
-                          AppColumnTextLayout(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            topText: "Mr ABDELILLAH",
-                            bottomText: "Passenger",
-                            color: Colors.grey.shade700,
-                            styleTwo: true,
-                          ),
-                          AppColumnTextLayout(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            topText: "434324423",
-                            bottomText: "Passport N°",
-                            color: Colors.grey.shade700,
-                            styleTwo: true,
-                          ),
-                        ],
-                      ),
-                      Container(
-                        padding: EdgeInsets.symmetric(vertical: 20),
-                        child: AppLayoutBuilder(
-                          randomDivider: 12,
-                          width: 4,
-                          color: AppTheme.bgColor,
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          AppColumnTextLayout(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            topText: "232324 4534235253656",
-                            bottomText: "E-Ticket N°",
-                            color: Colors.grey.shade700,
-                            styleTwo: true,
-                          ),
-                          AppColumnTextLayout(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            topText: "34GF35DS",
-                            bottomText: "Booking code",
-                            color: Colors.grey.shade700,
-                            styleTwo: true,
-                          ),
-                        ],
-                      ),
-                      Container(
-                        padding: EdgeInsets.symmetric(vertical: 20),
-                        child: AppLayoutBuilder(
-                          randomDivider: 12,
-                          width: 4,
-                          color: AppTheme.bgColor,
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Row(
+                              AppColumnTextLayout(
                                 crossAxisAlignment: CrossAxisAlignment.start,
+                                topText: "Mr ABDELILLAH",
+                                bottomText: "Passenger",
+                                color: Colors.grey.shade700,
+                                styleTwo: true,
+                              ),
+                              AppColumnTextLayout(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                topText: "785083421",
+                                bottomText: "Passport N°",
+                                color: Colors.grey.shade700,
+                                styleTwo: true,
+                              ),
+                            ],
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 20),
+                            child: AppLayoutBuilder(
+                              randomDivider: 12,
+                              width: 4,
+                              color: AppTheme.bgColor,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              AppColumnTextLayout(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                topText: "232324 4534235253656",
+                                bottomText: "E-Ticket N°",
+                                color: Colors.grey.shade700,
+                                styleTwo: true,
+                              ),
+                              AppColumnTextLayout(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                topText: "34GF35DS",
+                                bottomText: "Booking code",
+                                color: Colors.grey.shade700,
+                                styleTwo: true,
+                              ),
+                            ],
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 20),
+                            child: AppLayoutBuilder(
+                              randomDivider: 12,
+                              width: 4,
+                              color: AppTheme.bgColor,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
                                 children: [
-                                  Image.asset(
-                                    AppMedia.mastercard,
-                                    scale: 18,
+                                  Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Image.asset(
+                                        AppMedia.mastercard,
+                                        scale: 18,
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text(
+                                        "**** 4158",
+                                        style: AppTheme.headLineStyle3.copyWith(
+                                            fontFamily:
+                                                GoogleFonts.afacad().fontFamily,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black),
+                                      )
+                                    ],
                                   ),
-                                  SizedBox(
-                                    width: 10,
-                                  ),
-                                  Text(
-                                    "**** 4158",
-                                    style: AppTheme.headLineStyle3.copyWith(
-                                        fontFamily:
-                                            GoogleFonts.afacad().fontFamily,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.black),
+                                  StyledTextHeadlineFour(
+                                    text: "Payment method",
+                                    color: Colors.grey.shade700,
                                   )
                                 ],
                               ),
-                              StyledTextHeadlineFour(
-                                text: "Payment method",
+                              AppColumnTextLayout(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                topText: "€248.68",
+                                bottomText: "Price",
                                 color: Colors.grey.shade700,
-                              )
+                                styleTwo: true,
+                              ),
                             ],
-                          ),
-                          AppColumnTextLayout(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            topText: "€248.68",
-                            bottomText: "Price",
-                            color: Colors.grey.shade700,
-                            styleTwo: true,
                           ),
                         ],
                       ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 1,
-                ),
-                // barcode section
-                Container(
-                  width: 360,
-                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          bottomRight: Radius.circular(21),
-                          bottomLeft: Radius.circular(21))),
-                  child: Container(
-                    child: ClipRRect(
-                      child: BarcodeWidget(
-                          height: 80,
-                          data:
-                              "FULL NAME : MR FULL NAME | PASSPORT : 568495784",
-                          drawText: false,
-                          barcode: Barcode.qrCode()),
                     ),
-                  ),
-                )
-              ],
-            ),
+                    SizedBox(
+                      height: 1,
+                    ),
+                    // barcode section
+                    Container(
+                      width: 360,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(21),
+                              bottomLeft: Radius.circular(21))),
+                      child: Container(
+                        child: ClipRRect(
+                          child: BarcodeWidget(
+                              height: 80,
+                              data:
+                                  "FULL NAME : MR FULL NAME | PASSPORT : 568495784",
+                              drawText: false,
+                              barcode: Barcode.qrCode()),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 15,
+                ),
+                child: TicketView(
+                  ticket: ticketList[0],
+                  wholeScreen: true,
+                  isDefault: false,
+                ),
+              )
+            ],
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 15,
-            ),
-            child: TicketView(
-              ticket: ticketList[0],
-              wholeScreen: true,
-              isDefault: false,
-            ),
-          )
+          Positioned(
+              left: 15,
+              top: 400,
+              child: Container(
+                padding: EdgeInsets.all(3),
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, border: Border.all(width: 2)),
+                child: CircleAvatar(
+                  maxRadius: 4,
+                  backgroundColor: Colors.black,
+                ),
+              )),
+          Positioned(
+              right: 15,
+              top: 400,
+              child: Container(
+                padding: EdgeInsets.all(3),
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle, border: Border.all(width: 2)),
+                child: CircleAvatar(
+                  maxRadius: 4,
+                  backgroundColor: Colors.black,
+                ),
+              )),
         ],
       ),
     );
