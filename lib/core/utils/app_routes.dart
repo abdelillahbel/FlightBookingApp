2d7bc64a7ea3f92ab7a8ab770585b54e46c58f4d
abdelillahbel <73066920+abdelillahbel@users.noreply.github.com>
2025-01-11 12:32:20 +0100

    feat: add route for AllHotelsScreen in AppRoutes

--- lib/core/utils/app_routes.dart
+++ lib/core/utils/app_routes.dart
@@ -2,4 +2,5 @@ class AppRoutes {
   static const homeScreen = "/";
   static const allTicketsScreen = "/all_tickets_screen";
   static const ticketScreen = "/ticket_screen";
+  static const allHotelsScreen = "/all_hotels_screen";
 }
