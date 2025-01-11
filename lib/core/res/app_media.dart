63cbcf1fec89e51f882ead23b94c187d85719080
abdelillahbel <73066920+abdelillahbel@users.noreply.github.com>
2025-01-11 22:33:02 +0100

    feat: add SVG assets and new images to AppMedia

---
+++ lib/core/res/app_media.dart
@@ -0,0 +1,14 @@
+class AppMedia {
+  static const _baseImage = "assets/images";
+  static const logo = '$_baseImage/logo.png';
+  static const airplan1 = '$_baseImage/airplan1.png';
+  static const airAsia = '$_baseImage/air_asia.png';
+  static const flyWoman = '$_baseImage/fly_woman.png';
+  static const redWhiteMap = '$_baseImage/red_white_map.png';
+  static const tickets = '$_baseImage/tickets.png';
+  static const mastercard = '$_baseImage/mastercard.png';
+  static const airalgeria = '$_baseImage/airalgeria.png';
+  static const hotelRoom = '$_baseImage/hotel_room.png';
+  static const emojiLoveEyes =
+      '$_baseImage/smiling-face-with-heart-shaped-eyes.png';
+}
