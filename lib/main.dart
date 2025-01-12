636b636d97a7a352c7f412e44a8fa8f0c3177969
abdelillahbel <73066920+abdelillahbel@users.noreply.github.com>
2025-01-12 23:50:28 +0100

    feat: add HotelDetailsScreen route to main app navigation

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flightbookapp/core/res/styles/app_theme.dart';
 import 'package:flightbookapp/core/utils/app_routes.dart';
 import 'package:flightbookapp/screens/home/all_hotels_screen.dart';
 import 'package:flightbookapp/screens/home/all_tickets_screen.dart';
+import 'package:flightbookapp/screens/hotel/hotel_details_screen.dart';
 import 'package:flightbookapp/screens/ticket/ticket_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flightbookapp/core/bottom_nav_bar.dart';
@@ -31,7 +32,8 @@ class MyApp extends StatelessWidget {
         AppRoutes.homeScreen: (context) => const BottomNavBar(),
         AppRoutes.allTicketsScreen: (context) => const AllTicketsScreen(),
         AppRoutes.ticketScreen: (context) => const TicketScreen(),
-        AppRoutes.allHotelsScreen: (context) => const AllHotelsScreen()
+        AppRoutes.allHotelsScreen: (context) => const AllHotelsScreen(),
+        AppRoutes.hotelDetailsScreen: (context) => const HotelDetailsScreen()
       },
     );
   }
